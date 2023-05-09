1eb6394c0fa3dcc3e80c05e1e91feddf98f0615d
Krishna Pillaai Manogaran <kman0030@student.monash.edu>
2023-05-09 18:02:12 +1000

    Adding sql portfolio 2 file to oracle and git to begin work

---
+++ Applied/App09/SQLPortfolio2/sql_portfolio2_basic_intermediate.sql
@@ -0,0 +1,40 @@
+--Comment out SET ECHO and SPOOL commands before submitting your portfolio
+SET ECHO ON
+SPOOL sql_portfolio2_basic_intermediate_output.txt
+
+--****PLEASE ENTER YOUR DETAILS BELOW****
+--sql_portfolio2_basic_intermediate.sql
+
+--Student ID:
+--Student Name:
+--Unit Code:
+--Applied Class No:
+
+
+/*1*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon
+-- (;) at the end of this answer
+
+
+/*2*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon
+-- (;) at the end of this answer
+
+
+/*3*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon
+-- (;) at the end of this answer
+
+
+/*4*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon
+-- (;) at the end of this answer
+
+
+--Comment out SET ECHO and SPOOL commands before submitting your portfolio
+SPOOL OFF
+SET ECHO OFF
