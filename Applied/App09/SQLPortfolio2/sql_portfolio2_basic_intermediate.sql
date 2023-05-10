1111399580c5324f2e18bd4f9527b02ace5c641a
Krishna Pillaai Manogaran <kman0030@student.monash.edu>
2023-05-10 17:40:39 +1000

    Working on task 2 in SQL portfolio 2

--- Applied/App09/SQLPortfolio2/sql_portfolio2_basic_intermediate.sql
+++ Applied/App09/SQLPortfolio2/sql_portfolio2_basic_intermediate.sql
@@ -17,6 +17,7 @@ SPOOL sql_portfolio2_basic_intermediate_output.txt
 -- (;) at the end of this answer
 -- HOW TO PUT OWNER NAME TOGETHER
 -- MAINTANENCE DATE AND TIME
+/*
 SELECT
     maint_id, maint_datetime, prop_address, owner_givname, owner_famname, maint_desc, maint_cost
 FROM
@@ -25,38 +26,40 @@ WHERE
     maint_cost BETWEEN 1000 AND 3000 AND maint_assigned = 'Y'
 ORDER BY
     maint_cost DESC, maint_datetime;
-
+*/
 /*2*/
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
-/*
+
 SELECT 
     rent_agreement_no, tenant_no, tenant_title, tenant_givname, tenant_famname, prop_address, owner_givname, rent_lease_period
 FROM
-    (rent.rent JOIN rent.tenant ON rent.tenant_no = tenant.tenant_no) JOIN (rent.property JOIN rent.owner ON property.owner_no = owner.owner_no) ON property.prop_no = rent.prop_no
+    rent.tenant t JOIN rent.rent r ON t.tenant_no = r.tenant_no JOIN rent.property p ON r.prop_no = p.prop_no JOIN rent.owner o ON p.owner_no = o.owner_no
 WHERE
-    rent_weekly_rate < 425
+    rent_weekly_rate < 425 AND to_char(rent_lease_start, 'YYYY') = '2022'
 ORDER BY
-    tenant_no;
-*/
+    rent_lease_period, tenant_no;
+
 /*3*/
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
+/*
 SELECT
-    tenant_no, tenant_title, tenant_givname, tenant_famname, dam_no, dam_cost
+    tenant_no, tenant_title, tenant_givname, tenant_famname, COUNT(dam_no) AS no_of_damages, dam_cost
 FROM
     rent.tenant, rent.damage
 WHERE
-    to_char(dam_datetime, 'dd-Mon-yyyy') LIKE '%2022'
+    to_char(dam_datetime, 'YYYY') = '2022'
 ORDER BY
     dam_cost DESC, tenant_no;
-
+*/
 /*4*/
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
+/*
 SELECT
     tenant_no, tenant_title, tenant_givname, tenant_famname, prop_no, prop_address, COUNT(rent_agreement_no)
 FROM
@@ -65,7 +68,7 @@ WHERE
     UPPER(prop_address) LIKE '%TASMANIA'
 ORDER BY
     tenant_no;
-
+*/
 --Comment out SET ECHO and SPOOL commands before submitting your portfolio
 SPOOL OFF
 SET ECHO OFF
