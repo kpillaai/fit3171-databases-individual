cf4f2962e51a855b2cb7060d5aa314382fa2670d
Krishna Pillaai Manogaran <kman0030@student.monash.edu>
2023-05-17 16:56:14 +1000

    Adding SQL portfolio 3 files to git

---
+++ Applied/App10/SQLPortfolio3/sql_portfolio3_plsql.sql
@@ -0,0 +1,26 @@
+--Comment out SET ECHO, SET SERVEROUTPUT and SPOOL commands before submitting your portfolio
+SET ECHO ON
+SET SERVEROUTPUT ON
+SPOOL sql_portfolio3_plsql_output.txt
+
+--****PLEASE ENTER YOUR DETAILS BELOW****
+--sql_portfolio3_plsql.sql
+
+--Student ID:
+--Student Name:
+--Unit Code:
+--Applied Class No:
+
+-- PLEASE PLACE REQUIRED TRIGGER STATEMENT HERE
+-- ENSURE your trigger ends with a line break, then a slash (/) then another line break
+
+
+
+-- PLACE REQUIRED TRIGGER TEST HARNESS HERE
+
+
+
+
+--Comment out SET ECHO, SET SERVEROUTPUT and SPOOL commands before submitting your portfolio
+SPOOL OFF
+SET ECHO OFF
