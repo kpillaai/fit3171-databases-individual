5a7114d17c699eb8934a6900b7bedee8f1e7cc6f
Krishna Pillaai Manogaran <kman0030@student.monash.edu>
2023-05-20 20:45:27 +1000

    Working on task 1, completed Booking table

--- Assignments/Ass2A/T1-tsa-schema.sql
+++ Assignments/Ass2A/T1-tsa-schema.sql
@@ -18,15 +18,71 @@
 -- are included. FK constraints are to be added at the end of this script
 
 -- BOOKING
+CREATE TABLE booking (
+    booking_id                  NUMBER(8) NOT NULL,
+    resort_id                   NUMBER(4) NOT NULL,
+    cabin_no                    NUMBER(3) NOT NULL,
+    booking_from                DATE NOT NULL,
+    booking_to                  DATE NOT NULL,
+    booking_noadults            NUMBER(2) NOT NULL,
+    booking_nochildren          NUMBER(2) NOT NULL,
+    booking_total_points_cost   NUMBER(4) NOT NULL,
+    member_id                   NUMBER(6) NOT NULL,
+    staff_id                    NUMBER(5) NOT NULL
+);
+COMMENT ON COLUMN booking.booking_id IS
+    'Booking identifier, surrogate key added to replace BOOKING composite PK';
 
+COMMENT ON COLUMN booking.resort_id IS
+    'Resort identifier for this booking';
+    
+COMMENT ON COLUMN booking.cabin_no IS
+    'Cabin number within the resort';
+    
+COMMENT ON COLUMN booking.booking_from IS
+    'Date booking from';
+    
+COMMENT ON COLUMN booking.booking_to IS
+    'Date booking to';
 
+COMMENT ON COLUMN booking.booking_noadults IS
+    'Booking number of adults';
+    
+COMMENT ON COLUMN booking.booking_nochildren IS
+    'Booking number of children';
+    
+COMMENT ON COLUMN booking.booking_total_points_cost IS
+    'Total cost to the member in points for this booking';
+    
+COMMENT ON COLUMN booking.member_id IS
+    'Unique member id across TSA for member who made this booking';
+    
+COMMENT ON COLUMN booking.staff_id IS
+    'Staff identifier of staff member who took this booking';
+    
+ALTER TABLE booking ADD CONSTRAINT booking_pk PRIMARY KEY ( booking_id );
+
+ALTER TABLE booking ADD CONSTRAINT booking_nk UNIQUE ( booking_from, cabin_no, resort_id );
 
 -- CABIN
 
 
-
 -- Add all missing FK Constraints below here
-                        
+ALTER TABLE booking
+    ADD CONSTRAINT member_booking FOREIGN KEY ( resort_id )
+        REFERENCES member ( resort_id );   --CHECK THIS ONE
+        
+ALTER TABLE booking
+    ADD CONSTRAINT member_booking FOREIGN KEY ( member_id )
+        REFERENCES member ( member_id );
+    
+ALTER TABLE booking
+    ADD CONSTRAINT cabin_booking FOREIGN KEY ( cabin_no )
+        REFERENCES cabin ( cabin_no );
+
+ALTER TABLE booking
+    ADD CONSTRAINT staff_booking FOREIGN KEY ( staff_id )
+        REFERENCES staff ( staff_id );
    
 
 --**Trigger for checking and updating member.member_points for each booking.**--
