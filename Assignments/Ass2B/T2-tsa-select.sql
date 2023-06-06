42c03c8947de29423073d9333978aeac71cbc371
Krishna Pillaai Manogaran <kman0030@student.monash.edu>
2023-06-06 14:36:30 +1000

    Began work on task 2. Initial attempt on task 2a

--- Assignments/Ass2B/T2-tsa-select.sql
+++ Assignments/Ass2B/T2-tsa-select.sql
@@ -1,10 +1,10 @@
 --*****PLEASE ENTER YOUR DETAILS BELOW*****
 --T2-tsa-select.sql
 
---Student ID:
---Student Name:
---Unit Code:
---Applied Class No:
+--Student ID: 31500153
+--Student Name: Krishna Pillaai Manogaran
+--Unit Code: FIT3171
+--Applied Class No: 3
 
 /* Comments for your marker:
 
@@ -17,6 +17,19 @@
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
+SELECT t.town_id, t.town_name, pt.poi_type_id, pt.poi_type_descr, COUNT(*) AS poi_count
+FROM tsa.town t
+INNER JOIN tsa.point_of_interest poi ON t.town_id = poi.town_id
+INNER JOIN tsa.poi_type pt ON poi.poi_type_id = pt.poi_type_id
+WHERE t.town_id IN (
+    SELECT town_id
+    FROM tsa.point_of_interest
+    GROUP BY town_id
+    HAVING COUNT(DISTINCT poi_type_id) > 1
+)
+GROUP BY t.town_id, t.town_name, pt.poi_type_id, pt.poi_type_descr
+HAVING COUNT(*) > 1
+ORDER BY t.town_id, pt.poi_type_descr;
 
 
 
